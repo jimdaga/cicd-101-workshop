d0140281dae41d5a07b2ddbcfed004442ee229fb
Jim D'Agostino <dagosti@synopsys.com>
2018-10-28 22:03:09 -0400

    update test

--- test_hello_world.py
+++ test_hello_world.py
@@ -10,10 +10,10 @@ class TestHelloWorld(unittest.TestCase):
     def test_status_code(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
-    
+
     def test_message(self):
         response = self.app.get('/')
-        message = hello_world.wrap_html('Welcome to CI/CD 101 using CircleCI')
+        message = hello_world.wrap_html('Welcome to CI/CD 102 using CircleCI')
         self.assertEqual(response.data, message)
 
 if __name__ == '__main__':
